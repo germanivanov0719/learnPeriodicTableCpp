fb3d5f192b7824b01ad871dfa33082d99d2879d9
German Ivanov <germanivanov0719@gmail.com>
2021-12-22 02:20:45 +0300

    Initial Commit

---
+++ learnPeriodicTable/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learnPeriodicTable
+//
+//  Created by German Ivanov on 12/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
